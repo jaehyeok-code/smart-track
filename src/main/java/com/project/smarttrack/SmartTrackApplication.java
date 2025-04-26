b58ed4bff49fde7b57556ce9e9d26d0c7018802b
Jae-HyeokKim <jaehyeok.ethan@gmail.com>
2025-04-26 13:41:48 +0900

    Initial commit

---
+++ src/main/java/com/project/smarttrack/SmartTrackApplication.java
@@ -0,0 +1,13 @@
+package com.project.smarttrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTrackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SmartTrackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/project/smarttrack/SmartTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smarttrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTrackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
